3989e696ce3da139aa2565caec5e0c8c78e221f4
jepwebbe <jepwebbe@gmail.com>
2023-01-26 14:39:02 +0100

    regex contactform

--- src/Components/Pages/ContactForm.jsx
+++ src/Components/Pages/ContactForm.jsx
@@ -12,6 +12,7 @@ const ContactForm = () => {
     message: "",
   });
   // Updates the state whenever anything is written in the input field
+  const [isValid, setIsValid] = useState(true);
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData((prevFormData) => {
@@ -21,10 +22,20 @@ const ContactForm = () => {
         // [name]: type === "checkbox" ? checked : value
       };
     });
+    const emailRegex = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+    if (!emailRegex.test(value)) {
+      setIsValid(false);
+    } else {
+      setIsValid(true);
+    }
   };
+  
   // Uses Axios to post the formdata from the state to Strapi
   const handleSubmit = (event) => {
     event.preventDefault();
+    if (!isValid) {
+      return;
+    }
     axios
     // posts to imaginative MailChimp account
       .post("http://mailchimp.us8.list-manage.com/subscribe/post", {
@@ -51,6 +62,7 @@ const ContactForm = () => {
         maxLength="80"
         required
       />
+      {!isValid && <p>Indtast venligst en gyldig email-adresse</p>}
       <input
         onChange={handleChange}
         type="email"
@@ -58,8 +70,8 @@ const ContactForm = () => {
         value={formData.email}
         placeholder="Din email"
         required
-        pattern="/^\S+@\S+$/i"
       />
+          
       <textarea
         name="message"
         onChange={handleChange}
